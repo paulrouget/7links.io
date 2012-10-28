52f66af9cf8532dd6c5796abfb011398f7cfa82e
Paul Rouget <me@paulrouget.com>
2012-10-28 14:27:53 +0100

    update

--- local.js
+++ local.js
@@ -1,25 +1,3 @@
-const LOCAL_SYNC_INTERVAL = 10000;
-const REMOTE_SYNC_INTERVAL = 60000;
-
-const BUILTIN_ICONS = {
-  "twitter.com": "http://i.imgur.com/UJgWfs.png",
-  "mobile.twitter.com": "http://i.imgur.com/UJgWfs.png",
-  "facebook.com": "http://i.imgur.com/M0Wi5s.png",
-  "duckduckgo.com": "http://i.imgur.com/xEDQcs.png",
-  "news.ycombinator.com": "http://i.imgur.com/sAbeIs.png",
-  "ihackernews.com": "http://i.imgur.com/sAbeIs.png"
-}
-
-const DEFAULT_lINKS = [
-  {title: "Facebook", href: "http://facebook.com"},
-  {title: "r/comics", href: "http://imgur.com/r/comics"},
-  {title: "Hackernews", href: "http://ihackernews.com"},
-  {title: "DuckDuckGo", href: "http://duckduckgo.com"},
-  {title: "Twitter", href: "http://mobile.twitter.com"},
-  {title: "Reddit", href: "http://reddit.com/.compact"},
-  {title: ""}
-];
-
 var local = {};
 
 local.markDirty = function() {
